a45bfe00246fb62c9c1492ab909ba3cef2d4d99b
oobanooba0 <108059652+oobanooba0@users.noreply.github.com>
2026-03-21 21:27:52 +1100

    removed test code

--- data.lua
+++ data.lua
@@ -1,16 +1,2 @@
 require("prototypes.prototypes")
 require("prototypes.custom-inputs")
-
---this needs to be removed before release.
---i will forget this.
-data.raw.furnace.recycler.TFMG_thermal = {
-  max_working_temperature = 300,
-  max_safe_temperature = 450,
-  heat_ratio = 0.7,
-  connections = {
-    { position = {0.5, 1}, direction = 4},
-    --{ position = {0.5, -1}, direction = 4},
-    --{ position = {-0.5, 1}, direction = 12},
-    --{ position = {-0.5, -1}, direction = 12},
-  },
-}
